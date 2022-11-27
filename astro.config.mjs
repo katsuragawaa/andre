7cf30c9f1c0e8c04e6f49f733e6905c2d0e7a687
katsuragawa <andre.katsuragawa@gmail.com>
2022-11-27 14:08:04 -0300

    chore: remove react

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,6 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-import react from "@astrojs/react";
-
-// https://astro.build/config
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
@@ -12,7 +9,5 @@ import image from "@astrojs/image";
 // https://astro.build/config
 export default defineConfig({
     site: 'https://katsuragawaa.github.io',
-    integrations: [react(), tailwind(), image({
-        serviceEntryPoint: '@astrojs/image/sharp'
-    })]
+    integrations: [tailwind(), image({ serviceEntryPoint: '@astrojs/image/sharp' })]
 });
--- src/components/Hello.tsx
+++
@@ -1,5 +0,0 @@
-const Hello = () => {
-    return <h1 className="text-4xl font-bold text-center">Hello</h1>;
-};
-
-export default Hello;
